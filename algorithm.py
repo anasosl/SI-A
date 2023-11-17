7e958854fcc4fa86b07b66933652ce9e34ae4ad5
anasosl <108680344+anasosl@users.noreply.github.com>
2023-11-17 08:10:39 -0300

    Add files via upload

---
+++ algorithm.py
@@ -0,0 +1,5 @@
+import data
+
+def A(e_origin, e_destination):
+    o = data.get_number(e_origin)
+    d = data.get_number(e_destination)
---
+++ main.py
@@ -0,0 +1,9 @@
+import algorithm
+
+estado_origem = input("\nEm qual estação você está? (ex. E12, 12 ou e12)\n")
+
+estado_destino = input("\nPara qual estação você deseja ir? (ex. E15, 15 ou e15)\n")
+
+estado_destino.split(' ')
+
+melhor_caminho = algorithm.A(estado_origem, estado_origem)
